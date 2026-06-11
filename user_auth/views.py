54a0c36b50e903117ea13d941b67517e2105918f
argho7 <sahaarghya2002@gmail.com>
2026-06-11 17:07:28 +0600

    fix login

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def user_login(request):
-    if request.method=='POST':
+    if request.user.is_authenticated:
+        return redirect('home')
+    elif request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, f'Login Successful! Welcome back {username.upper()}')
             return redirect('home')
+        else:
+            messages.error(request, 'Wrong username or password!')
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
