4c36603b3957c6d08eb0d1f3858bd21bca297bf1
argho7 <sahaarghya2002@gmail.com>
2026-06-21 21:37:48 +0600

    add tinymce configuration

--- blog/settings.py
+++ blog/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'tinymce',
+    
     'user_auth',
     'blogpost',
     
-    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,32 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+# TinyMCE Configuration
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 500,
+    'width': '100%',
+    'menubar': 'file edit view insert format tools table help',
+    'plugins': '''
+        advlist autolink lists link image charmap print preview anchor
+        searchreplace visualblocks code fullscreen
+        insertdatetime media table paste code help wordcount
+        ''',
+    'toolbar': '''
+        undo redo | bold italic underline strikethrough | 
+        forecolor backcolor | alignleft aligncenter alignright | 
+        bullist numlist | link image media | 
+        code fullscreen preview | help
+        ''',
+    'toolbar_mode': 'sliding',
+    'contextmenu': 'link image table',
+    'images_upload_url': '/upload-image/',  # For image upload
+    'images_upload_handler': 'handle_image_upload',  # Custom handler
+    'relative_urls': False,
+    'remove_script_host': False,
+    'convert_urls': True,
+    'browser_spellcheck': True,
+    'paste_data_images': True,
+    'autoresize_bottom_margin': 50,
+    'autoresize_on_init': True,
+}
